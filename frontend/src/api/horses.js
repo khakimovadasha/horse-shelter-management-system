99c1d51b356fdf4b94736e4bd5831c6c100a9757
khakimovadasha <catdog2161718@gmail.com>
2026-04-07 23:16:02 +0300

    axios

---
+++ frontend/src/api/horses.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+})
+
+export const getHorses = async () => {
+  const response = await api.get('/horses/')
+  return response.data
+}

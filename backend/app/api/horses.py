e7a551e7cd93832b403488ada9909cc4f1daf7b3
khakimovadasha <catdog2161718@gmail.com>
2026-04-07 21:24:30 +0300

    + api get horse_id

--- backend/app/api/horses.py
+++ backend/app/api/horses.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -14,3 +14,14 @@ def get_horses(db: Session = Depends(get_db)):
     result = db.execute(select(Horse).order_by(Horse.id.desc()))
     horses = result.scalars().all()
     return horses
+
+
+@router.get("/{horse_id}", response_model=HorseRead)
+def get_horse(horse_id: int, db: Session = Depends(get_db)):
+    result = db.execute(select(Horse).where(Horse.id == horse_id))
+    horse = result.scalar_one_or_none()
+
+    if horse is None:
+        raise HTTPException(status_code=404, detail="Horse not found")
+
+    return horse

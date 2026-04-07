fee7454c81dc6e5a43e732aa0f0ff0ce4d030466
khakimovadasha <catdog2161718@gmail.com>
2026-04-07 22:13:34 +0300

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.horses import router as horses_router
 
 app = FastAPI(title="Horse Shelter API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:9000",
+        "http://127.0.0.1:9000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(horses_router, prefix="/api")
 app.mount("/media", StaticFiles(directory="media"), name="media")

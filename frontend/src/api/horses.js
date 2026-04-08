46c8b29564c3f0fb00904e88b3416640c8390593
khakimovadasha <catdog2161718@gmail.com>
2026-04-08 22:08:54 +0300

    get  horses id

--- frontend/src/api/horses.js
+++ frontend/src/api/horses.js
@@ -4,7 +4,14 @@ const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/api',
 })
 
+export const API_BASE_URL = 'http://127.0.0.1:8000'
+
 export const getHorses = async () => {
   const response = await api.get('/horses/')
+  return response.data
+}
+
+export const getHorseById = async (horseId) => {
+  const response = await api.get(`/horses/${horseId}`)
   return response.data
 }

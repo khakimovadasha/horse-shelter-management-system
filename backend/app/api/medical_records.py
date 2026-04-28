22e897fbac43b4ba7cf812cf47f54639caa7d07c
khakimovadasha <catdog2161718@gmail.com>
2026-04-28 17:32:56 +0300

    добавление мед.записи

--- backend/app/api/medical_records.py
+++ backend/app/api/medical_records.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from app.api.auth import get_current_user
+from datetime import datetime
+from app.api.auth import get_current_user, require_admin_or_veterinarian
 from app.db.session import get_db
 from app.models.medical_record import MedicalRecord
 from app.models.user import User
-from app.schemas.medical_record import MedicalRecordRead
+from app.schemas.medical_record import MedicalRecordRead, MedicalRecordCreate
 
 router = APIRouter(prefix="/horses", tags=["Medical Records"])
 
@@ -24,3 +25,26 @@ def get_medical_records_by_horse(
     ).scalars().all()
 
     return records
+
+
+@router.post("/{horse_id}/medical-records", response_model=MedicalRecordRead)
+def create_medical_record(
+    horse_id: int,
+    data: MedicalRecordCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(require_admin_or_veterinarian),
+):
+    new_record = MedicalRecord(
+        horse_id=horse_id,
+        record_date=datetime.utcnow(),
+        record_type=data.record_type,
+        title=data.title,
+        description=data.description,
+        next_procedure_date=data.next_procedure_date,
+    )
+
+    db.add(new_record)
+    db.commit()
+    db.refresh(new_record)
+
+    return new_record
--- backend/app/schemas/medical_record.py
+++ backend/app/schemas/medical_record.py
@@ -1,22 +1,27 @@
 from datetime import datetime
 from pydantic import BaseModel
 
+from app.models.medical_record import MedicalRecordType
+
 
 class MedicalRecordRead(BaseModel):
     id: int
     horse_id: int
     record_date: datetime
-    record_type: str
+    record_type: MedicalRecordType
     title: str
     description: str
     next_procedure_date: datetime | None
     created_at: datetime
     updated_at: datetime
 
+    model_config = {
+        "from_attributes": True
+    }
+
 
 class MedicalRecordCreate(BaseModel):
-    record_date: datetime
-    record_type: str
+    record_type: MedicalRecordType
     title: str
     description: str
     next_procedure_date: datetime | None = None

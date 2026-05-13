58ff23b553a654590b18e01d9ab3e3d75ac945cf
khakimovadasha <catdog2161718@gmail.com>
2026-05-13 22:52:46 +0300

    компоненты для редактирования и удаления

---
+++ frontend/src/components/ui/AppDeleteAction/AppDeleteAction.vue
@@ -0,0 +1,30 @@
+<template>
+  <AppButton
+    color="white"
+    text-color="negative"
+    outline
+    no-caps
+    icon="delete"
+    label=""
+    :dense="dense"
+    :class="$style.button"
+    v-bind="$attrs"
+  />
+</template>
+
+<script setup>
+import AppButton from 'src/components/ui/AppButton/AppButton.vue'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+defineProps({
+  dense: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<style module lang="scss" src="./AppDeleteAction.module.scss"></style>

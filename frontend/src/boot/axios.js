318d6af2342dad1125a0c9a87d40210c93f3374e
khakimovadasha <catdog2161718@gmail.com>
2026-05-21 21:20:23 +0300

    api url

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -9,7 +9,11 @@ import { useCurrentUserStore } from 'src/stores/currentUser'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000'
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.PROD
+    ? 'https://shelter-api-dashakhakimova.amvera.io'
+    : 'http://127.0.0.1:8000')
 
 const api = axios.create({ baseURL: `${API_BASE_URL}/api` })
 
